023c8b0105efbd5a2b356c9bcc76d15545e1c1da
JasonLee-p <2593292614@qq.com>
2024-03-17 06:22:09 +0800

    pep8

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 import numpy as np
 from PyQt5 import QtCore, QtGui, QtWidgets
 from pyqtOpenGL import GLViewWidget
-from pyqtOpenGL.items  import *
+from pyqtOpenGL.items import *
 from pyqtOpenGL.transform3d import *
 import PIL.Image as Image
 from time import time
@@ -22,19 +22,19 @@ class GLView(GLViewWidget):
         self.box = GLBoxTextureItem(size=(2, 2, 2)).translate(0, 1.1, 0)
         self.text = GLTextItem(text="Hello World", pos=(2, 6, -1), color=(1, 0.6, 1), fixed=False)
         self.arrow = GLArrowPlotItem(
-            start_pos=ver1+[5,-1,0],
-            end_pos=ver1+norm1+[5,-1,0],
-            color=[1,1,0]
+            start_pos=ver1 + [5, -1, 0],
+            end_pos=ver1 + norm1 + [5, -1, 0],
+            color=[1, 1, 0]
         )
 
         # -- scatter and line
-        pos = np.random.uniform(-2, 2, size=(15, 3)).astype('f4')*(2, 1, 2) + [0, -3, 0]
+        pos = np.random.uniform(-2, 2, size=(15, 3)).astype('f4') * (2, 1, 2) + [0, -3, 0]
         color = np.random.uniform(0, 1, size=(15, 3)).astype('f4')
         self.scatter = GLScatterPlotItem(pos=pos, color=color, size=2)
         self.line = GLLinePlotItem(pos=pos, color=color, lineWidth=2)
 
         # -- lights
-        self.light = PointLight(pos=[0, 5, 4], ambient=(0.8, 0.8, 0.8),diffuse=(0.8, 0.8, 0.8))
+        self.light = PointLight(pos=[0, 5, 4], ambient=(0.8, 0.8, 0.8), diffuse=(0.8, 0.8, 0.8))
         self.light1 = PointLight(pos=[0, -5, 1], diffuse=(0, .8, 0))
         self.light2 = PointLight(pos=[-12, 3, 2], diffuse=(0.8, 0, 0))
 
@@ -52,12 +52,12 @@ class GLView(GLViewWidget):
 
         # -- mesh
         self.mesh1 = GLInstancedMeshItem(
-            pos=[[5,-1,0], [-3,5,-5], [4,6,-8]],
+            pos=[[5, -1, 0], [-3, 5, -5], [4, 6, -8]],
             lights=[self.light, self.light1, self.light2],
             indices=ind1,
             vertexes=ver1,
             normals=norm1,
-            color=(0.7,0.8,0.8)
+            color=(0.7, 0.8, 0.8)
         )
 
         self.mesh2 = GLMeshItem(
@@ -68,23 +68,23 @@ class GLView(GLViewWidget):
         ).rotate(-50, 1, 0.4, 0).translate(-6, 2, -2)
 
         # -- surface
-        self.zmap = np.random.uniform(0, 1.5, (25,25))
+        self.zmap = np.random.uniform(0, 1.5, (25, 25))
         self.texture = Texture2D(sin_texture(0))
         self.surf = GLSurfacePlotItem(
             zmap=self.zmap, x_size=6, lights=[self.light, self.light1, self.light2],
-            material= Material((0.2, 0.2, 0.2), diffuse=(0.5, 0.5, 0.5), textures=[self.texture])
+            material=Material((0.2, 0.2, 0.2), diffuse=(0.5, 0.5, 0.5), textures=[self.texture])
         ).rotate(-90, 1, 0, 0).translate(-6, -1, 0)
 
         # -- 3d grid
-        z = np.random.uniform(-3, -2, (5,6))
+        z = np.random.uniform(-3, -2, (5, 6))
         y = np.arange(5) + 2
         x = np.arange(6) + 1
         X, Y = np.meshgrid(x, y, indexing='xy')
         grid = np.stack([X, Y, z], axis=2)
-        color = np.random.random((5,6, 3))
+        color = np.random.random((5, 6, 3))
         self.grid3d = GL3DGridItem(grid=grid, fill=True, opacity=0.5, color=color)
         self.grid3d.setDepthValue(10)
-        self.grid3d1 = GL3DGridItem(grid=grid, fill=False, color=(0,0,0))
+        self.grid3d1 = GL3DGridItem(grid=grid, fill=False, color=(0, 0, 0))
         self.grid3d1.setDepthValue(-1)
 
         self.addItem(self.grid3d)
@@ -114,17 +114,19 @@ class GLView(GLViewWidget):
         self.texture.updateTexture(sin_texture(time()))
         self.update()
 
+
 def sin_texture(t):
     delta = t % 100
     x = np.linspace(-10, 10, 50, dtype='f4')
     y = x.copy()
     X, Y = np.meshgrid(x, y, indexing='xy')
-    Z = (np.sin(np.sqrt(X**2 + Y**2) * np.pi / 5 - delta*np.pi) + 1) / 5
+    Z = (np.sin(np.sqrt(X ** 2 + Y ** 2) * np.pi / 5 - delta * np.pi) + 1) / 5
     return np.stack([Z, Z, Z], axis=2)
 
 
 if __name__ == '__main__':
     import sys
+
     app = QtWidgets.QApplication(sys.argv)
     win = GLView(None)
     win.show()
